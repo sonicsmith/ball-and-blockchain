373eab0492054c5e30f1c9ba987689c51f4959e1
sonicsmith <thesonicsmith@gmail.com>
2018-03-26 11:13:11 +1300

    About restyled.

--- src/js/components/About.jsx
+++ src/js/components/About.jsx
@@ -6,15 +6,15 @@ export default class About extends React.Component {
     return (
       <div className="center-all">
         <h2>
-          Create a permanent, immutable record of your love, on the
-          blockchain.
+          Create a permanent, immutable record of your love, <div />
+          on the blockchain.
         </h2>
         <div className="aboutText">
           <div>
 
             <p>Ball and Blockchain is a decentralised application running on the Ethereum network.<br />
               By entering in the details of you and your partner, you are able to make a permanent,
-              immutable record of your partnership for a small payment of Ether.
+              immutable record of your partnership.
               Once a Ball and Blockchain “marriage certificate“ is created, it cannot be destroyed.
               The certificate cannot be changed by the address holder, creators of Ball and Blockchain,
               or a even a third party.
@@ -23,12 +23,18 @@ export default class About extends React.Component {
               This record could be used to mark a real marriage, or as a replacement to conventional
               legal / religious marriage. It could be given as a present to a couple, or used as a way
               to mark a wedding anniversary.
-              <div />
-              The public address attached to the certificate can continue to be used as a functional
-              ethereum address. It could also be used as the address wedding donations like a wishing well.
             </p>
-            <div style={{ padding: 20 }}>
-              <Link to="/create">CREATE NOW</Link>
+            <p>
+              The public address attached to the certificate can continue to be used as a functional
+              ethereum address. It could even be used as the address for wedding donations like a wishing well.
+            </p>
+            <div>
+              <span style={{ padding: 20 }}>
+                <Link to="/create">CREATE NOW</Link>
+              </span>
+              <span style={{ padding: 20 }}>
+                <Link to="/address/0x950a2D2076fC8c23FBCa1E2a9752D38e377a2a4c">SEE EXAMPLE</Link>
+              </span>
             </div>
 
           </div>
